285b89ccb90e3248c0858b17eb4aaec5e410b7cd
chiffa <andrei.chiffa136@gmail.com>
2015-02-16 09:43:51 -0600

    Added removed global switches from the wrappers

--- src/chiffatools/wrappers.py
+++ src/chiffatools/wrappers.py
@@ -3,26 +3,21 @@ __author__ = 'ank'
 import matplotlib.pyplot as plt
 from time import time
 
-global debug
-global timing
-
 def rs(matrix, name):
     plt.title(name)
     plt.imshow(matrix, interpolation='nearest')
     plt.colorbar()
-    if debug:
-        plt.show()
-    plt.clf()
+    plt.show()
+
 
 def debug_wrapper(funct):
 
     def check_matrix(*args,**kwargs):
         result = funct(*args, **kwargs)
-        if debug:
-            if type(result) is not tuple:
-                rs(result, funct.__name__)
-            else:
-                rs(result[0], funct.__name__)
+        if type(result) is not tuple:
+            rs(result, funct.__name__)
+        else:
+            rs(result[0], funct.__name__)
         check_matrix.__name__ = funct.__name__
         check_matrix.__doc__ = funct.__doc__
         return result
@@ -34,8 +29,7 @@ def time_wrapper(funct):
     def time_execution(*args,**kwargs):
         start = time()
         result = funct(*args, **kwargs)
-        if timing:
-            print funct.__name__, time()-start
+        print funct.__name__, time()-start
         time_execution.__doc__ = funct.__doc__
         return result
 
