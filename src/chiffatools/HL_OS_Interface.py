f9001f94d716364f1bd85ec6b4dd420b642f0834
chiffa <andrei.chiffa136@gmail.com>
2015-04-08 19:02:51 -0500

    Added high-level interface routines

---
+++ src/chiffatools/HL_OS_Interface.py
@@ -0,0 +1,24 @@
+__author__ = 'ank'
+
+import os, shutil
+
+
+def rm_f(folder):
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception, e:
+            print e
+
+
+def rm_rf(folder):
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception, e:
+            print e
